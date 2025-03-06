779fe875ecc6b1060aa0e21b14f146cad8457d49
Nathália Venega <159180447+nathvng@users.noreply.github.com>
2025-03-06 12:47:12 -0300

    Add files via upload

---
+++ jjj 06.03.25/db_joalheriajoiasjoia.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE IF NOT EXISTS db_joalheriajoiasjoia;
+
+DROP DATABASE db_joalheriajoiasjoia;
+
+USE db_joalheriajoiasjoia;
+
+desc tb_cliente;
+SELECT * FROM tb_cliente;
+
+desc tb_endereco;
+SELECT * FROM tb_endereco;
+
+desc tb_produto;
+SELECT * FROM tb_produto;
